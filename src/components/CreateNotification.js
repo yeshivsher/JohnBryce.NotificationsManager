2541df95d8ab1209bcd14a63b1ad824281bed8f4
RaphaelArtlist <raphael.b@artlist.io>
2022-02-03 20:22:37 +0200

    aaa

--- src/components/CreateNotification.js
+++ src/components/CreateNotification.js
@@ -1,7 +1,31 @@
+import { makeStyles } from '@mui/styles';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-const CreateNotification  = () => {
+const useStyles = makeStyles({
+    root:{
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    paper: {
+        marginTop: 200,
+        width: 300,
+        height: 200
+    }
+});
+
+const CreateNotification = () => {
+    const classes = useStyles()
+
     return (
-        <p>CreateNotification</p>
+        <div className={classes.root}>
+            <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Create new notification
+                    </Typography>
+            </Paper>
+        </div>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,51 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom'
+import { makeStyles } from '@mui/styles';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useSelector } from 'react-redux';
+
+const useStyles = makeStyles({
+    links: {
+        width: '30%',
+        display: 'flex', 
+        justifyContent: 'space-between'
+    },
+    link: {
+        color: 'white',
+        textDecoration: 'none',
+        fontSize: 18
+    }
+});
+
 
 const Navbar = () => {
+    const classes = useStyles();
+
+    const notifications = useSelector(state => state.navbar.notifications)
+
     return (
-        <Box sx={{ flexGrow: 1}}>
+        <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Notificatoins System
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <div className={classes.links}>
+                        <Link
+                            className={classes.link}
+                            to="/home"
+                        >Home</Link>
+                        <Link
+                            className={classes.link}
+                            to="/createnotification"
+                        >Create</Link>
+                        <Badge badgeContent={notifications.length} color="error">
+                            <NotificationsIcon />
+                        </Badge>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>)
