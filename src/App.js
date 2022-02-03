f3eade7ad5298d4d388c11ae71fe3ee88494b959
RaphaelArtlist <raphael.b@artlist.io>
2022-02-03 19:38:07 +0200

    finished routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// locals components
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import CreateNotification from './components/CreateNotification';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/createnotification" element={<CreateNotification />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/CreateNotification.js
@@ -0,0 +1,8 @@
+
+const CreateNotification  = () => {
+    return (
+        <p>CreateNotification</p>
+    )
+}
+
+export default CreateNotification
