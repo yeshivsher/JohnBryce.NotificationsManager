46ed1c5d035376f2f787683927d42a56be1713bf
RaphaelArtlist <raphael.b@artlist.io>
2022-02-03 21:11:47 +0200

    aaa

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,45 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-
-const Home  = () => {
+const Home = () => {
     return (
-        <p>Home</p>
+        <List dense={true}>
+            {
+                [1, 2, 3, 4].map(element => {
+                    return <ListItem
+                        secondaryAction={
+                            <IconButton edge="end" aria-label="delete">
+                                <DeleteIcon />
+                            </IconButton>
+                        }
+                    >
+                        <ListItemAvatar>
+                            <Avatar>
+                                <FolderIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Single-line item"
+                        />
+                    </ListItem>
+                })
+            }
+        </List>
     )
 }
 
