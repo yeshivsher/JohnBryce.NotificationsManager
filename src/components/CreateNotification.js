07184865b0579827a0e7e5a1a46c7cbcf166d5e8
RaphaelArtlist <raphael.b@artlist.io>
2022-02-03 20:29:48 +0200

    next step

--- src/components/CreateNotification.js
+++ src/components/CreateNotification.js
@@ -1,17 +1,24 @@
 import { makeStyles } from '@mui/styles';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const useStyles = makeStyles({
-    root:{
+    root: {
         width: '100vw',
         display: 'flex',
         justifyContent: 'center'
     },
     paper: {
+        padding: 10,
         marginTop: 200,
         width: 300,
-        height: 200
+        height: 200,
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
     }
 });
 
@@ -21,9 +28,11 @@ const CreateNotification = () => {
     return (
         <div className={classes.root}>
             <Paper elevation={3} className={classes.paper}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Create new notification
-                    </Typography>
+                <Typography variant="h6" component="div">
+                    Create new notification
+                </Typography>
+                <TextField id="outlined-basic" label="Message" variant="outlined" />
+                <Button variant="contained">Create</Button>
             </Paper>
         </div>
     )
